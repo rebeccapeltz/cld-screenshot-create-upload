06b62d63a51af09875ae62becf292170ce230bdb
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-12-21 17:14:36 -0800

    try binary

--- public/script.js
+++ public/script.js
@@ -1,56 +1,42 @@
 function bufferToImageUrl(buffer) {
   // See https://gist.github.com/candycode/f18ae1767b2b0aba568e
 
-  var arrayBufferView = new Uint8Array(buffer);
-  var blob = new Blob([arrayBufferView], { type: "image/jpeg" });
+  var arrayBufferView = new Uint8Array( buffer );
+  var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
   var urlCreator = window.URL || window.webkitURL;
-  var imageUrl = urlCreator.createObjectURL(blob);
+  var imageUrl = urlCreator.createObjectURL( blob );
 
   return imageUrl;
 }
 
-document
-  .querySelector('button[type="submit"]')
-  .addEventListener("click", (e) => {
-    e.preventDefault();
+document.querySelector('button[type="submit"]').addEventListener('click', (e) => {
 
-    const pageToScreenshot = document.getElementById("page").value;
+  e.preventDefault();
 
-    if (!pageToScreenshot)
-      return (document.getElementById("result").textContent =
-        "Please enter a page URL");
+  const pageToScreenshot = document.getElementById('page').value;
 
-   
-    const options = {
+  if (!pageToScreenshot) return document.getElementById('result').textContent = 'Please enter a page URL';
+
+  const options = {
       method: "POST",
       headers: { "Content-Type": "application/json; charset=utf-8" },
-      body: JSON.stringify({"pageToScreenshot":pageToScreenshot}),
-    };
+      body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
+  };
 
-    document.getElementById("result").textContent = "Please wait...";
+  document.getElementById('result').textContent = "Please wait..."
 
-    fetch(
-      ".netlify/functions/screenshot",
-      options
-    )
+  fetch("/.netlify/functions/screenshot", options)
+      .then((res) => res.json())
       .then((res) => {
-        console.log(res);
-        res.json();
-      })
-      .then((res) => {
-        if (!res.buffer)
-          return (document.getElementById("result").textContent =
-            "Error capturing screenshot");
+          
+          if (!res.buffer) return document.getElementById('result').textContent = 'Error capturing screenshot';
 
-        // const img = document.createElement("img");
-        img.src = bufferToImageUrl(res.buffer.data);
-        img.src = res.buffer.data;
-        document.getElementById("result").innerHTML = img.outerHTML;
+          const img = document.createElement('img');
+          img.src = bufferToImageUrl(res.buffer.data);
+          document.getElementById('result').innerHTML = img.outerHTML;
       })
       .catch((err) => {
-        console.log(err);
-        document.getElementById(
-          "result"
-        ).textContent = `Error: ${err.toString()}`;
+          console.log(err)
+          document.getElementById('result').textContent = `Error: ${err.toString()}`
       });
-  });
+});
